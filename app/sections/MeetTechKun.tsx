324b0c61ac448c08e6f7cdb33df13ae13c18c666
Naved M. Usmani <navedm1424@gmail.com>
2026-04-14 08:31:56 +0530

    Modifications

--- app/sections/MeetTechKun.tsx
+++ app/sections/MeetTechKun.tsx
@@ -68,8 +68,8 @@ export default function MeetTechKun() {
             position: absolute;
             inset: 0;
             background: radial-gradient(
-                ellipse var(--page-width) 62.5% at 50% 50%,
-                transparent,
+                ellipse var(--page-width) 75% at 50% 50%,
+                transparent 12.5%,
                 var(--secondary-950) 62.5%,
                 transparent
             );
