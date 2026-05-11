8f5dd3001252ef7f5c965efb4737c8ebe4090c2f
Naved M. Usmani <navedm1424@gmail.com>
2026-05-11 14:05:34 +0530

    Modifications.

--- app/components/RadialReveal.tsx
+++ app/components/RadialReveal.tsx
@@ -1,7 +1,8 @@
 import {motion, useTransform} from "motion/react";
 import React from "react";
 import {MotionValue} from "motion";
-import {Angle} from "svg-path-kit";
+import {Angle, Point2D, Vector2D} from "svg-path-kit";
+import {round} from "svg-path-kit/numbers";
 
 export type RadialRevealProps = {
 	angle: MotionValue<Angle>;
@@ -13,26 +14,46 @@ export type RadialRevealProps = {
 	backLayer?: SvgChild | SvgChild[];
 };
 
+const roundOff = (num: number) => round(num, 4);
+
 type SvgChild = React.ReactElement<React.SVGProps<SVGElement>>;
 export default function RadialReveal({ angle, startAngle: startAngleValue = 0, centerX, centerY, radius, frontLayer, backLayer }: RadialRevealProps) {
-	const startAngle = Angle.of(startAngleValue);
+	const center = Point2D.of(centerX, centerY);
+	const startPolarVector = Vector2D.polar(radius, startAngleValue);
+	const backClipPath = useTransform(
+		angle,
+		a => {
+			const arcEnd = center.add(startPolarVector);
+			return `
+			M ${center.x} ${center.y}
+			l ${roundOff(radius * a.cosine)} ${roundOff(radius * a.sine)}
+			A ${radius} ${radius} 0 ${+a - startAngleValue > Math.PI ? 0 : 1} 1 ${roundOff(arcEnd.x)} ${roundOff(arcEnd.y)}
+			`;
+		}
+	);
 	const frontClipPath = useTransform(
 		angle,
-		(a) => {
+		a => {
 			return `
-			M ${centerX} ${centerY}
-			l ${radius * startAngle.cosine} ${radius * startAngle.sine}
-			A ${radius} ${radius} 0 ${+a - +startAngle > Math.PI ? 1 : 0} 1 ${centerX + radius * a.cosine} ${centerY + radius * a.sine}
+			M ${center.x} ${center.y}
+			l ${roundOff(startPolarVector.x)} ${roundOff(startPolarVector.y)}
+			A ${radius} ${radius} 0 ${+a - startAngleValue > Math.PI ? 1 : 0} 1 ${roundOff(centerX + radius * a.cosine)} ${roundOff(centerY + radius * a.sine)}
 			`;
 		}
 	);
 
 	return <>
 		<defs>
+			<clipPath id="back-clip-path">
+				<motion.path d={backClipPath}></motion.path>
+			</clipPath>
 			<clipPath id="front-clip-path">
 				<motion.path d={frontClipPath}></motion.path>
 			</clipPath>
 		</defs>
+		<g clipPath="url(#back-clip-path)">
+			{backLayer}
+		</g>
 		<g clipPath="url(#front-clip-path)">
 			{frontLayer}
 		</g>
