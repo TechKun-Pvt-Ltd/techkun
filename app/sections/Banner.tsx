8f7914ada8b2179f94c231ffefb99ca9f2ebaf83
Naved M. Usmani <navedm1424@gmail.com>
2026-05-21 06:04:49 +0530

    Modifications.

--- app/sections/Banner.tsx
+++ app/sections/Banner.tsx
@@ -1,8 +1,9 @@
 'use client'
-import React from "react";
+import React, {useId} from "react";
 import {css} from "@emotion/react";
 
 export default function Banner() {
+    const id = useId();
     return <section>
         <div css={css`
             display: flex;
@@ -13,31 +14,40 @@ export default function Banner() {
         `}>
             <h1 className="hero-heading">
                 We build software that's<br/>
-                <span css={css`
-                    font-size: var(--font-size-8xl);
-                    letter-spacing: var(--letter-spacing-8xl);
-                    line-height: var(--line-height-8xl);
+                <span className="text-8xl" css={css`
+                    //color: var(--background);
+                    //-webkit-text-stroke: 1px var(--foreground);
+                `}>robust</span><br/>
+                <span className="text-8xl" css={css`
+                    //color: var(--primary-400);
+                    font-style: italic;
+                    font-weight: 600;
                 `}>
-                    robust,<br/>
-                    <span css={css`
-                        position: relative;
-                        isolation: isolate;
-                        //color: var(--primary-400);
-                        font-style: italic;
-                        font-weight: 600;
+                    <svg css={css`
+                        width: 3.56ch;
                     `}>
                         {Array.from({ length: 4 }).map((_, i) =>
-                            <span style={{
-                                opacity: 0.5 - i * 0.125,
-                                position: "absolute",
-                                left: -(i + 1) * 10,
-                                zIndex: -(i + 1)
-                            }}>f</span>
+                            <text
+                                key={i}
+                                opacity={(i + 1) * 0.125}
+                                x={-10 + i * 10} y="var(--font-size-8xl)"
+                                fill="var(--neutral-200)"
+                            >f</text>
                         )}
-                        fast
-                    </span>,<br/>
-                    & delightful.
-                </span>
+                        <defs>
+                            <mask id={`text-mask-${id}`}>
+                                <rect fill="white" x="0" y="%" width="100%" height="100%"/>
+                                <text x={-10 + 40} y="var(--font-size-8xl)">fast</text>
+                            </mask>
+                        </defs>
+                        <text x={-10 + 40} mask={`url(#text-mask-${id})`} strokeWidth="3" stroke="var(--foreground)" y="var(--font-size-8xl)">fast</text>
+                        <text x={-10 + 40} fill="var(--background)" y="var(--font-size-8xl)">fast</text>
+                    </svg>
+                </span><br/>
+                & <span className="text-8xl" css={css`
+                    //color: var(--background);
+                    //-webkit-text-stroke: 1px var(--foreground);
+                `}>delightful</span>
             </h1>
         </div>
     </section>;
