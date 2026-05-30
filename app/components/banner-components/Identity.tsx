4029918d74df435446f8f61bca1b87d10267dc2b
Naved M. Usmani <navedm1424@gmail.com>
2026-05-30 05:45:32 +0530

    Modifications.

--- app/components/banner-components/Identity.tsx
+++ app/components/banner-components/Identity.tsx
@@ -94,7 +94,7 @@ export default function Identity({style, ...props}: React.ComponentPropsWithoutR
 					d="M 0 0 C 0 -2.7614 2.2386 -5 5 -5 C 7.7614 -5 10 -2.7614 10 0 C 10 1.3261 9.4732 2.5979 8.5355 3.5355 C 7.5979 4.4732 7.0711 5.745 7.0711 7.0711 C 7.0711 7.6234 6.6234 8.0711 6.0711 8.0711 L 3.9289 8.0711 C 3.3766 8.0711 2.9289 7.6234 2.9289 7.0711 C 2.9289 5.745 2.4021 4.4732 1.4645 3.5355 C 0.5268 2.5979 0 1.3261 0 0"
 				/>
 			</svg>
-			<svg className="icon dots" xmlns="http://www.w3.org/2000/svg">
+			<svg className="icon dots" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
 				{Array.from({length: DOT_COUNT}).map((_, i) => (
 					<circle
 						key={i}
