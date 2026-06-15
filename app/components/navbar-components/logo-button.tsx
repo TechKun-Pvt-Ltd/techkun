02398922acaedd0d64348bec7938de497fa6159e
Naved M. Usmani <navedm1424@gmail.com>
2026-06-15 19:05:18 +0530

    Modifications.

--- app/components/navbar-components/logo-button.tsx
+++ app/components/navbar-components/logo-button.tsx
@@ -1,9 +1,27 @@
 import {css} from "@emotion/react";
 import React, {useEffect, useRef, useState} from "react";
 import TechKunLogo from "@/app/components/techkun-logo";
-import {motion} from "motion/react";
+import {
+	AnimatePresence,
+	calcGeneratorDuration,
+	maxGeneratorDuration,
+	motion,
+	spring,
+	visualElementStore
+} from "motion/react";
+import {generateLinearEasing} from "motion";
 
-export default function LogoButton({style, ...props}: React.ComponentProps<typeof motion.button>) {
+const springGenerator = spring(undefined, 0.1);
+const calculatedDuration = Math.min(calcGeneratorDuration(springGenerator), maxGeneratorDuration);
+const springLinearEasing = generateLinearEasing(
+	(progress) => springGenerator.next(calculatedDuration * progress).value,
+	calculatedDuration,
+	30
+);
+
+const EXIT_DURATION = 0.4;
+
+export default function LogoButton({className, style, ...props}: React.ComponentProps<typeof motion.button>) {
 	const textElement = useRef<HTMLSpanElement>(null);
 	const textHovered = useRef(false);
 	const textAboveThreshold = useRef(true);
@@ -13,22 +31,17 @@ export default function LogoButton({style, ...props}: React.ComponentProps<typeo
 		setTextState("enter");
 	}
 	function animateOut() {
+		// the text element cannot be animated out by other events if it's hovered or above the threshold.
 		if (textAboveThreshold.current || textHovered.current) return;
+
+		const presenceContext = visualElementStore.get(textElement.current)?.presenceContext!;
+		presenceContext.register("exiting-text-" + presenceContext.id);
 		setTextState("exit");
 	}
 
 	useEffect(() => {
-		if (textState === "exit") {
-			if (!textElement.current) return;
-
-			const element = textElement.current;
-			const activeTransition = element.getAnimations()
-				.filter(anim => anim instanceof CSSTransition)
-				.find(t => t.transitionProperty === "background-position-x");
-
-			if (!activeTransition)
-				setTextState("unmounted");
-		}
+		if (textState === "exit")
+			setTextState("unmounted");
 	}, [textState]);
 
 	useEffect(() => {
@@ -46,17 +59,24 @@ export default function LogoButton({style, ...props}: React.ComponentProps<typeo
 
 	return <motion.button
 		layout="size"
-		className="display-text"
-		style={{ borderRadius: '16px', ...style }}
+		className={"display-text " + className}
+		style={{ borderRadius: "16px", ...style }}
+		transition={textState === "unmounted" ? { duration: EXIT_DURATION + 0.1 }: undefined}
 		css={css`
 			padding-block: 16px;
 			padding-inline: 24px;
-			corner-shape: squircle;
 			background-color: oklch(from var(--background) 0.14 c h);
 			display: flex;
 			align-items: center;
 			gap: 16px;
 			font-weight: 500;
+			position: relative;
+
+			//corner-shape: squircle;
+			//border-radius: 64px;
+			//@supports not (corner-shape: superellipse(2)) {
+			//	border-radius: 8px;
+			//}
 		`}
 		onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
 		{...props}
@@ -72,36 +92,45 @@ export default function LogoButton({style, ...props}: React.ComponentProps<typeo
 		<motion.span layout>
 			<TechKunLogo style={{ display: "block" }} />
 		</motion.span>
-		{textState !== "unmounted" && <span
-			ref={textElement}
-            css={css`
-				color: transparent;
-				background-image: linear-gradient(
-					to right in oklch,
-					var(--foreground) 33.33%,
-					var(--secondary-500),
-					var(--primary-500),
-					transparent 66.66%
-				);
-				text-box-trim: trim-both;
-				background-size: 300% 100%;
-				background-clip: text;
-				background-position-y: 50%;
-
-				transition: background-position-x 0.6s ease-in-out;
-				background-position-x: 0;
-				&[data-state="enter"] {
-					@starting-style {
+		<AnimatePresence mode="popLayout">
+			{textState !== "unmounted" && <motion.span
+				ref={textElement}
+				layout
+				css={css`
+					color: transparent;
+					background-image: linear-gradient(
+						to right in oklch,
+						var(--foreground) 33.33%,
+						var(--secondary-500),
+						var(--primary-500),
+						transparent 66.66%
+					);
+					text-box-trim: trim-both;
+					background-size: 300% 100%;
+					background-clip: text;
+					background-position-y: 50%;
+	
+					transition: background-position-x 0.6s ease-in-out;
+					background-position-x: 0;
+					&[data-state="enter"] {
+						@starting-style {
+							background-position-x: 100%;
+						}
+					}
+					&[data-state="exit"] {
+						transition-timing-function: ${springLinearEasing};
+						transition-duration: ${EXIT_DURATION}s;
 						background-position-x: 100%;
-                    }
-				}
-				&[data-state="exit"] {
-					transition-duration: 0.3s;
-					background-position-x: 100%;
-				}
-			`}
-            data-state={textState}
-            onTransitionEnd={() => textState === "exit" && setTextState("unmounted")}
-        >TechKun</span>}
+					}
+				`}
+				data-state={textState}
+				onTransitionEnd={_ => {
+					if (textState !== "exit") return;
+
+					const presenceContext = visualElementStore.get(textElement.current)?.presenceContext!;
+					presenceContext.onExitComplete?.("exiting-text-" + presenceContext.id);
+				}}
+			>TechKun</motion.span>}
+		</AnimatePresence>
 	</motion.button>;
 };
