f8bffd43326e7f620f1a0de37857082f06024ffc
Naved M. Usmani <navedm1424@gmail.com>
2026-07-23 20:16:05 +0530

    Added anticipation to the banner animation.

--- app/components/banner-components/Identity.tsx
+++ app/components/banner-components/Identity.tsx
@@ -212,7 +212,7 @@ export default forwardRef<IdentityRef, React.ComponentPropsWithoutRef<"span">>(f
 			`}>
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
-					width="24" height="24" viewBox="0 0 24 24"
+					width="0.275em" viewBox="0 0 24 24"
 					stroke="currentColor" fill="currentColor"
 					strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
 					className="pointer"
