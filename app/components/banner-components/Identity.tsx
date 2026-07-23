7ecd96a97c11171d29edfa2c1f587afa05b508b6
Naved M. Usmani <navedm1424@gmail.com>
2026-07-23 21:25:05 +0530

    Added anticipation to the banner animation.

--- app/components/banner-components/Identity.tsx
+++ app/components/banner-components/Identity.tsx
@@ -68,11 +68,59 @@ export default forwardRef<IdentityRef, React.ComponentPropsWithoutRef<"span">>(f
 	}), []);
 
 	return <span
-		style={{color: 'var(--foreground)', display: "inline-block", ...style} as React.CSSProperties}
+		style={{color: 'var(--foreground)', position: "relative", display: "inline-block", ...style} as React.CSSProperties}
 		{...props}
 		ref={spanRef}
 		data-lights-off
 	>
+		<span css={css`
+			position: absolute;
+			color: var(--neutral-100);
+			offset-path: shape(
+				from calc(100% + 0.2em) 25%,
+				curve to calc(100% - 0.25em) 110% with 0.6em 25% from start / 1em 0 from end,
+				curve to 0.05em 70% with -1em 0 from start / 0.15em 0 from end
+			);
+			offset-distance: 0;
+			offset-anchor: center center;
+			offset-rotate: -30deg;
+			@keyframes move {
+				from {
+					offset-distance: 0;
+				}
+				to {
+					offset-distance: 99%;
+					offset-rotate: 0deg;
+				}
+			}
+			@keyframes move-back {
+				from {
+					offset-distance: 99%;
+				}
+				to {
+					offset-distance: 0;
+					offset-rotate: -30deg;
+					transform: translate(0);
+				}
+			}
+			--stretch-index: 0.5;
+			[data-initial] & {
+				animation:
+					move ${pointerMove.duration}s ease-in-out both,
+					stretch ${dotsStretch.duration}s ${dotsStretch.delay}s ${stretchTimingFunction} both,
+					move-back ${pointerMoveBack.duration}s ${pointerMoveBack.delay}s ease-in-out forwards;
+			}
+		`}>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="0.275em" viewBox="0 0 24 24"
+				stroke="currentColor" fill="currentColor"
+				strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+				className="pointer"
+			>
+				<path d="M 11.572 3.866 a 0.495 0.495 45 0 1 0.9207 -0 l 6.7175 15.9099 a 0.5 0.5 45 0 1 -0.7142 0.6251 l -5.4476 -3.2131 a 2 2 45 0 0 -2.0315 -0.0021 l -5.4483 3.2152 a 0.5 0.5 45 0 1 -0.7142 -0.6251 z" />
+			</svg>
+		</span>
 		<span css={css`
 			position: relative;
 			color: transparent;
@@ -172,54 +220,6 @@ export default forwardRef<IdentityRef, React.ComponentPropsWithoutRef<"span">>(f
 					/>
 				))}
 			</svg>
-			<span css={css`
-				position: absolute;
-				color: var(--neutral-100);
-				offset-path: shape(
-					from calc(100% + 0.2em) 25%,
-					curve to calc(100% - 0.25em) 110% with calc(100% + 0.8em) 50% / calc(100% + 0.75em) 110%,
-					curve to 0.05em 70% with calc(100% - 1em) 110% / 0.2em 70%
-				);
-				offset-distance: 0;
-				offset-anchor: center center;
-				offset-rotate: -30deg;
-				@keyframes move {
-					from {
-						offset-distance: 0;
-					}
-					to {
-						offset-distance: 99%;
-						offset-rotate: 0deg;
-					}
-				}
-				@keyframes move-back {
-					from {
-						offset-distance: 99%;
-					}
-					to {
-						offset-distance: 0;
-						offset-rotate: -30deg;
-						transform: translate(0);
-					}
-				}
-				--stretch-index: 0.5;
-				[data-initial] & {
-					animation:
-						move ${pointerMove.duration}s ease-in-out both,
-						stretch ${dotsStretch.duration}s ${dotsStretch.delay}s ${stretchTimingFunction} both,
-						move-back ${pointerMoveBack.duration}s ${pointerMoveBack.delay}s ease-in-out forwards;
-				}
-			`}>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="0.275em" viewBox="0 0 24 24"
-					stroke="currentColor" fill="currentColor"
-					strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-					className="pointer"
-				>
-					<path d="M 11.572 3.866 a 0.495 0.495 45 0 1 0.9207 -0 l 6.7175 15.9099 a 0.5 0.5 45 0 1 -0.7142 0.6251 l -5.4476 -3.2131 a 2 2 45 0 0 -2.0315 -0.0021 l -5.4483 3.2152 a 0.5 0.5 45 0 1 -0.7142 -0.6251 z" />
-				</svg>
-			</span>
 			i
 		</span>
 		dentity
