f2af6483036f89c3f895df9f55f5c90b1a3d6adc
Naved M. Usmani <navedm1424@gmail.com>
2026-09-09 08:43:20 +0530

    SEO improvements.

--- app/photos/social-card/SocialCard.tsx
+++ app/photos/social-card/SocialCard.tsx
@@ -5,18 +5,19 @@ import logoPath from "@/public/logo-path.json";
 import {TECHKUN_LOGO_PATH_HREF} from "@/app/Shared.tsx";
 import React from "react";
 
+const width = 360;
 /** This component is not supposed to use any theme variables. It is supposed to be a pure image. */
 export default function SocialCard() {
     return <div style={{ ...SOCIAL_CARD_SIZE, paddingBlock: "36px", display: "flex", flexDirection: "column", alignItems: "center", gap: "16px", backgroundColor: "#030304" }}>
         <svg
-            width="360" height="auto"
+            width={width} height={width * logoPath.viewBox.height / logoPath.viewBox.width}
             viewBox={viewBoxString(logoPath.viewBox)}
         >
             <use href={TECHKUN_LOGO_PATH_HREF} fill="#1572db" />
         </svg>
         <div style={{ textAlign: "center", display: "flex", flexDirection: "column", alignItems: "center", color: "#dddee1" }}>
-            <h1 style={{ letterSpacing: "0.06em", fontSize: "56px", marginBlockEnd: "0.2em" }}>TechKun</h1>
-            <p style={{ fontSize: "24px", color: "#a2a9cb" }}>We build software with beauty,<br/>precision, and identity.</p>
+            <h1 style={{ margin: 0, letterSpacing: "0.06em", fontSize: "56px", marginBlockEnd: "0.2em" }}>TechKun</h1>
+            <p style={{ margin: 0, fontSize: "24px", color: "#a2a9cb" }}>We build software with beauty,<br/>precision, and identity.</p>
         </div>
     </div>;
 }
